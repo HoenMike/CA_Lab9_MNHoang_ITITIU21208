04b907544b906ddcd87c3dca9037b486e62d787f
HoènMike <hoanggg003@gmail.com>
2024-06-03 22:32:44 +0700

    update comment

--- Ex2.s
+++ Ex2.s
@@ -9,54 +9,56 @@ lineBreak: .asciiz "\n"
 
 main:
   # Setup stack frame
-  addi $sp, $sp, -4      # Adjust $sp
-  sw $ra, 0($sp)         # Save $ra
+  addi $sp, $sp, -4      # Decrease stack pointer by 4
+  sw $ra, 0($sp)         # Store return address on stack
 
   # Call arrayLength
-  la $a0, Array     # Load array address
-  jal arrayLength     # Call subroutine
+  la $a0, Array          # Load base address of Array into $a0
+  jal arrayLength        # Jump to arrayLength subroutine
 
   # Call arraySort
-  la $a0, Array     # Load array address
-  move $a1, $v0          # Move length to $a1
-  jal arraySort               # Call subroutine
+  la $a0, Array          # Load base address of Array into $a0
+  move $a1, $v0          # Move length of array into $a1
+  jal arraySort          # Jump to arraySort subroutine
 
   # Print sorted array
-  la $a0, Array     # Load array address
-  move $a1, $v0          # Move length to $a1
-  jal printSortedArray         # Call subroutine
+  la $a0, Array          # Load base address of Array into $a0
+  move $a1, $v0          # Move length of array into $a1
+  jal printSortedArray   # Jump to printSortedArray subroutine
 
   # Cleanup stack
-  lw $ra, 0($sp)         # Restore $ra
-  addi $sp, $sp, 4       # Restore $sp
+  lw $ra, 0($sp)         # Load return address from stack
+  addi $sp, $sp, 4       # Increase stack pointer by 4
 
   # Terminate program
-  li $v0, 10             # Exit syscall
-  syscall
+  li $v0, 10             # Load syscall number for exit
+  syscall                # Perform syscall (exit)
 
 arrayLength:
-  li $v0, 0              # Initialize length
-  li $t0, 0              # Initialize index
+  li $v0, 0              # Initialize length counter to 0
+  li $t0, 0              # Initialize index counter to 0
 
-loop:
-  mul $t1, $t0, 4        # Calculate index
-  add $t1, $t1, $a0      # Get array address
-  lwc1 $f1, 0($t1)       # Load array value
-  c.eq.s $f1, $f0        # Compare with 0.0
-  bc1t exit              # Exit if value is 0.0
-  addi $t0, $t0, 1       # Increase index
-  addi $v0, $v0, 1       # Increase length
-  j loop                 # Repeat loop
+lengthLoop:
+  mul $t1, $t0, 4        # Calculate offset for current index
+  add $t1, $t1, $a0      # Add offset to base address of Array
+  lwc1 $f1, 0($t1)       # Load float at calculated address
+  c.eq.s $f1, $f0        # Compare loaded float with 0.0
+  bc1t lengthExit        # If float is 0.0, branch to lengthExit
+  addi $t0, $t0, 1       # Increase index counter
+  addi $v0, $v0, 1       # Increase length counter
+  j lengthLoop           # Jump back to start of loop
 
-exit:
-  jr $ra                 # Return
+lengthExit:
+  jr $ra                 # Return to caller
+
+# ... rest of the code ...
 
 arraySort:
   add $t0, $zero, $zero  # Initialize counter1
 
 outerLoop:
   addi $t0, $t0, 1       # Increase i
-  bgt $t0, $a1, endloop1 # Break if i >= size
+  bgt $t0, $a1, endOuterLoop # Break if i >= size
   add $t1, $a1, $zero    # Initialize counter2
 
 innerLoop:
@@ -69,11 +71,12 @@ innerLoop:
   lwc1 $f5, 0($t7)       # Load array[j]
   lwc1 $f6, 0($t8)       # Load array[j-1]
   c.le.s $f5, $f6        # Compare array[j] <= array[j-1]
-  bc1t innerLoop             # Continue if array[j] <= array[j-1]
+  bc1t innerLoop         # Continue if array[j] <= array[j-1]
   swc1 $f5, 0($t8)       # Swap array[j] and array[j-1]
   swc1 $f6, 0($t7)
-  j innerLoop                # Repeat innerLoop
-endloop1:
+  j innerLoop            # Repeat innerLoop
+
+endOuterLoop:
   jr $ra                 # Return
 
 printSortedArray:
@@ -88,7 +91,7 @@ printLoop:
   li $v0, 4
   la $a0, lineBreak
   syscall                # Print space
-  
+
   addi $t0, $t0, 4       # Move to next element
   addi $t1, $t1, 1       # Increase index
   blt $t1, $a1, printLoop# Repeat until all elements printed
